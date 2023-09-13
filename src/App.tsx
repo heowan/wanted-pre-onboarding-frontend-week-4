9caa71366b9908e70c465b37f636b2f5d0330f12
heowan <101536766+heowan@users.noreply.github.com>
2023-09-13 18:33:05 +0900

    feat: 필터링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import ChartDiv from './component/chart/Chart';
+import { useState } from 'react';
+import Filters from './component/filter/filter';
 
 function App() {
+	const [activeTab, setActiveTab] = useState<string>('');
+
 	return (
 		<div>
-			<ChartDiv />
+			<Filters activeTab={activeTab} setActiveTab={setActiveTab} />
+			<ChartDiv activeTab={activeTab} />
 		</div>
 	);
 }
